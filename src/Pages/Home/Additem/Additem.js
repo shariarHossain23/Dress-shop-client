b6ea7145a79328c39fcd660e86ae6cc0c4d83f71
shariarhossain23e <shariarhossain23@gamil.comm>
2022-04-29 16:51:54 +0600

    add item done

--- src/Pages/Home/Additem/Additem.js
+++ src/Pages/Home/Additem/Additem.js
@@ -1,24 +1,60 @@
+import axios from "axios";
 import React from "react";
-import './additem.css';
+import { Button, Form } from "react-bootstrap";
+import { toast } from "react-toastify";
+import PageTitle from "../../Shared/PageTItle/PageTitle";
+import "./additem.css";
 
 const Additem = () => {
   // dressName, price, img,supplierName, quantity,desc
-
+  const handleAddItem = (event) => {
+    event.preventDefault();
+    const dressCollection = {
+      dressName: event.target.product.value,
+      price: event.target.price.value,
+      img: event.target.imgUrl.value,
+      desc: event.target.desc.value,
+      supplierName: event.target.supplierName.value,
+      quantity: event.target.quantity.value,
+    };
+    axios.post("http://localhost:5000/dress",dressCollection)
+    .then(response => {
+        toast.success("product upload successfully")
+        event.target.reset()
+    })
+  };
   return (
     <div>
       <div className="container-fluid ">
         <div className="row">
           <div className="col-md-6 mx-auto form-group">
-              <h3 className="mb-4">Add Your Product</h3>
-            <form action="">
-              <input type="text" name="product" id="" placeholder="ProductName" required />
-              <input type="number" name="price" id="" placeholder="price" required />
-              <input type="text" name="imgUrl" id="" placeholder="img-url" required />
-              <input type="text" name="desc" id="" placeholder="description" required />
-              <input type="text" name="supplierName" id="" placeholder="supplier Name" required />
-              <input type="text" name="quantity" id="" placeholder="quantity" required />
-             <input type="button" className="inventory-btn" value="Submit" />
-            </form>
+              <PageTitle title='additem'></PageTitle>
+            <h3 className="mb-4">Add Your Product</h3>
+            <Form onSubmit={handleAddItem} className="">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control name="product" type="text" placeholder="Product Name" required/>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control name="price" type="number" placeholder="Price" required />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control name="imgUrl" type="text" placeholder="img-url" required />
+              </Form.Group>
+              <Form.Group className="mb-2" controlId="formBasicEmail">
+                <Form.Control name="desc" type="text" placeholder="Description" required />
+              </Form.Group>
+              <Form.Group className="mb-2" controlId="formBasicEmail">
+                <Form.Control name="supplierName" type="text" placeholder="supplierName" required />
+              </Form.Group>
+              <Form.Group className="mb-2" controlId="formBasicEmail">
+                <Form.Control name="quantity" type="text" placeholder="Quantity" required />
+              </Form.Group>
+            <div className="text-center">
+            <Button className="inventory-btn" variant="primary" type="submit">
+                Submit
+              </Button>
+            </div>
+            </Form>
           </div>
         </div>
       </div>
